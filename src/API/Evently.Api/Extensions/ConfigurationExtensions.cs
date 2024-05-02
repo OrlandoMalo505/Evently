16c132553a2f6403484066ec58452423bcbcae35
Orlando Malo <orlandomalo0@gmail.com>
2024-05-02 20:07:46 +0200

    added module configuration

---
+++ src/API/Evently.Api/Extensions/ConfigurationExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace Evently.Api.Extensions;
+
+internal static class ConfigurationExtensions
+{
+    internal static void AddModuleConfiguration(this IConfigurationBuilder configuration, string[] modules)
+    {
+        foreach (var module in modules)
+        {
+            configuration.AddJsonFile($"modules.{module}.json", false, true);
+            configuration.AddJsonFile($"modules.{module}.Development.json", true, true);
+        }
+    }
+}
